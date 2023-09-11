ee18a4855785f3f5717b6533fe854865a78cce18
mmanuel <bateemma14@gmail.com>
2023-09-11 14:09:15 +0100

    added slider for the mid section

--- src/components/RootElement.js
+++ src/components/RootElement.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import NavBar from './NavBar'
 import SideBar from './SideBar'
-// import Slider from './Slider'
 import useToggleCartSlider from '../utils/useToggleCartSlider'
 import CartModal from './CartModal'
 import NotificationPopUp from './NotificationPopup'
 
 const RootElement = () => {
-    const { isOpen: active, close } = useToggleCartSlider()
-    console.log("this is the active state", active)
+    const { isOpen: active } = useToggleCartSlider()
     const [isOpen, setIsOpen] = useState(false)
     return (
         <>
